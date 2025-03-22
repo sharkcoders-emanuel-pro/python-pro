3054069da75b50e01dfca01f735c4a424dc3d62e
sharkcoders-emanuel-pro <shark07868@sharkcoders.pt>
2025-03-22 17:14:42 +0000

    Add files via upload

--- dictionary_phones_2.py
+++ dictionary_phones_2.py
@@ -3,10 +3,13 @@
 def get_contact(contacts,name):
    return contacts[name]
 
-def set_contact(contacts,name,phone_number):
-    contacts[name] = phone_number
+def register_contact(contacts,name,number):
+    if number not in contacts:
+        contacts[name] = number
+    else:
+        print("The contact already exists.")
 
-def remove_contact(contacts,name):
+def remove_contact(contacts, name):
     contacts.pop(name)
 
 def list_contacts(contacts):
@@ -35,10 +38,34 @@ def main():
     option = 0
     while option != 5:
         option = main_menu()
-        if(option == 1):
-            print(f"{set_contacts(name)}")
+        if option == 1 :
+            name = input("Please insert the name of the contact: ")
+            print(get_contact(contacts, name))
+
+        elif option == 2 :
+            name = input("Please insert the name of the contact: ")
+            phone_number = int(input("Please insert the name of the contact: "))
+            register_contact(contacts, name, phone_number)
+            print("The contact was successfully added!")
+            print(list_contacts(contacts))
+
+        elif option == 3:
+            name = input("Please insert the name of the contact: ")
+            remove_contact(contacts, name)
+            print("The contact was successfully removed!")
+            print(list_contacts(contacts))
+
+        elif option == 4:
+            print(list_contacts(contacts))
+
+        elif option == 5:
+            print("Sair")
+
+        else:
+            print("Opção inválida!")
+
 
 
 
 name = input("Insert the name of the Contact you Want: ")
-print(f"The Number is: {get_contacts(name)}")
+print(f"The Number is: {get_contact(name)}")
